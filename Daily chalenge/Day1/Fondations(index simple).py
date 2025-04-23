682946f43def1d72f295001e540669d48bdf9aba
Allcodln <traoreibrahimkhalil006@gmail.com>
2025-04-23 12:22:13 -0500

    Day1-ended

---
+++ Daily chalenge/Day1/Fondations(index simple).py
@@ -0,0 +1,22 @@
+mot = "Python"
+
+print("\n Affiche la première lettre du mot")
+print(f"\n {mot[0]}")
+
+print("\n Affiche la deuxième lettre")
+print(f"\n {mot[1]}")
+
+print("\n Affiche la dernière lettre")
+print(f"\n {mot[-1]}")
+
+print("\n Affiche les trois premières lettres")
+print(f"\n {mot[:3]}")
+
+print("\n Affiche les trois dernières lettres")
+print(f"\n {mot[-3:]}")
+
+print("\n Affiche le mot en }ordre inversé")
+print(f"\n {mot[::-1]}")
+
+print("\n Affiche uniquement les lettres y, t, o (avec les index)")
+print(f"\n {mot[1], mot[2], mot[4]}")
---
+++ Daily chalenge/Day1/slicing simple(start-end).py
@@ -0,0 +1,13 @@
+phrase = "Apprendre Python est amusant"
+
+print("\n Affiche les 9 premiers caractères")
+print(f"\n {phrase[:9]}")
+
+print("\n Affiche le mot 'Python' avec un slicing")
+print(f"\n {phrase[10:17]}")
+
+print("\n Affiche tout sauf le premier et dernier caractère")
+print(f"\n {phrase[1:-1]}")
+
+print("\n Affiche le mot 'amusant' uniquement avec slicing")
+print(f"\n {phrase[-7:]}")
