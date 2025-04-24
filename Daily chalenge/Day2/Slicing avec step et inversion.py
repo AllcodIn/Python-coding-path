4030d6b46acb48c6ade3db3c5be67f5fc13cc148
AllcodIn <156183920+AllcodIn@users.noreply.github.com>
2025-04-24 18:03:46 +0000

    Day2-ended

---
+++ Daily chalenge/Day2/Slicing avec step et inversion.py
@@ -0,0 +1,13 @@
+mot =  "programmation"
+
+print("\n 1.Affiche un caractère sur deux")
+print(f"\n {mot[::2]}")
+
+print("\n 2.Affiche les lettres aux index pairs")
+print(f"\n {mot[0::2]}")
+
+print("\n 3.Inverse le mot")
+print(f"\n {mot[::-1]}")
+
+print("\n 4.Affiche les 5 premiers caractères, en inversé")
+print(f"\n {mot[:5][::-1]}")
